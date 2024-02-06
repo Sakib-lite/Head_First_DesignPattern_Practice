24e04bd6d70578726e4bc7a51d03a6e593721200
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2024-02-07 00:27:13 +0600

    decorator pattern practice

---
+++ Decorator_Pattern/Espresso.java
@@ -0,0 +1,14 @@
+public class Expresso implements Bevarage {
+
+
+    String name = "Expresso";
+
+    public String getDescription() {
+        return name;
+    }
+
+
+    public double cost() {
+        return 10;
+    }
+}
